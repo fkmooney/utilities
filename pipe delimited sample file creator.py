63a1493b36f99932162281a68c8e69aa22068c8c
Kempton Mooney <fkmooney@users.noreply.github.com>
2021-12-30 11:04:14 -0500

    Update pipe delimited sample file creator.py

--- pipe delimited sample file creator.py
+++ pipe delimited sample file creator.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-df = pd.read_csv('BNN-bnwkreport2021.txt', sep='|')
-df = df.drop(columns=['BNCollege','BN.com','BarnesNoble'])
+df = pd.read_csv('wkreport2021.txt', sep='|')
+df = df.drop(columns=['College','.com','Other'])
 df["TotalRetail"] = df["TotalRetail"].astype(int)
 
-df.to_csv('AMZ_Studios_Sample.txt', sep='|', index=False)
+df.to_csv('Sample.txt', sep='|', index=False)
