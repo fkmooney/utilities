e03818b18148123b27f5ea3de68dcf38ffaa771d
Kempton Mooney <fkmooney@users.noreply.github.com>
2021-12-30 11:01:29 -0500

    Update big txt or json file opener.py

--- big txt or json file opener.py
+++ big txt or json file opener.py
@@ -7,7 +7,6 @@ def read_big_file(filename):
              print(line)
 
 f = "C:/Users/Kempton Mooney/Desktop/bookscan_weekly.json"  # amazon json file name
-#f = 'Extended_DMA_201832.txt'  # extended dma file name
-#f = "V:\Bookscan\BookScan 'Offline Reports' Repository\Early View reports\Approved reports\PRH\PRH-extendeddmasansnbYYYYWW.txt"
+
 
 read_big_file(f) 
