e57ac5abd22286ef4ec8549c8c1f04b6f409b27a
Kempton Mooney <fkmooney@users.noreply.github.com>
2021-12-30 11:02:19 -0500

    Update create sample from big txt file.py

--- create sample from big txt file.py
+++ create sample from big txt file.py
@@ -1,9 +1,8 @@
 ### This can be used to open big json files one line at a time. 
 ### It can be used to open other big .txt files.
 
-#f = "bookscan_weekly.json"  # amazon json file name
-#f = "//pscnas/CustDeliverables/BookScan/CustomGeo/output_files/PRH-extended_dma_sansnb_201830.txt"
-f = 'PRH-extended_dma_sansnb_201830.txt'  # extended dma file name
+
+f = 'extended_dma_sansnb_201830.txt'  # extended dma file name
 
 with open(f) as myfile:
     head = [next(myfile) for x in range(2000)]
